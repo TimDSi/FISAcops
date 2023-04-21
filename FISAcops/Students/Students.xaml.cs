e728db53f1529668c7d9e44adbc381af7a9c2e5d
TimDSi <theotime.dsilva1505@gmail.com>
2023-04-21 19:33:58 +0200

    ajout de la fonctionalité de suppression et début de celle d'édition

--- FISAcops/Students/Students.xaml.cs
+++ FISAcops/Students/Students.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -26,11 +28,87 @@ namespace FISAcops
             mainWindow.frame.Navigate(new MainPage());
         }
 
+        private void EditStudent_Click(object sender, RoutedEventArgs e)
+        {
+            // Récupérer l'élève sélectionné
+            var selectedStudent = ((Button)sender).Tag as Student;
+            /*
+            // Ouvrir la fenêtre d'édition de l'élève
+            var editStudentWindow = new CreateStudent(selectedStudent);
+            editStudentWindow.ShowDialog();
+
+            // Mettre à jour la liste des étudiants si l'utilisateur a cliqué sur "Enregistrer"
+            if (editStudentWindow.DialogResult == true)
+            {
+                // Récupérer l'élève modifié
+                var editedStudent = editStudentWindow.Student;
+
+                // Rechercher et remplacer l'élève modifié dans la liste des étudiants
+                var students = studentsListView.ItemsSource as List<Student>;
+                var index = students.FindIndex(s => s.Id == editedStudent.Id);
+                students[index] = editedStudent;
+
+                // Rafraîchir la liste des étudiants
+                studentsListView.ItemsSource = null;
+                studentsListView.ItemsSource = students;
+
+                // Enregistrer les modifications dans le fichier JSON
+                SaveStudentsToJson(students);
+            }*/
+        }
+
+        private void DeleteStudent_Click(object sender, RoutedEventArgs e)
+        {
+            // Récupérer l'élève sélectionné
+            var selectedStudent = ((Button)sender).Tag as Student;
+
+            // Demander confirmation à l'utilisateur avant de supprimer l'élève
+            var result = MessageBox.Show($"Voulez-vous vraiment supprimer l'élève {selectedStudent.Nom} ?", "Confirmation de suppression", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                // Supprimer l'élève de la liste des étudiants
+                var students = studentsListView.ItemsSource as List<Student>;
+                students.Remove(selectedStudent);
+
+                // Rafraîchir la liste des étudiants
+                studentsListView.ItemsSource = null;
+                studentsListView.ItemsSource = students;
+
+                // Enregistrer les modifications dans le fichier JSON
+                SaveStudentsToJson(students);
+            }
+        }
+
+        private void SaveStudentsToJson(List<Student> students)
+        {
+            // Convertir la liste des étudiants en JSON
+            string json = JsonSerializer.Serialize(students);
+
+            // Enregistrer le JSON dans le fichier "students.json"
+            string appLocation = @"D:\Projets\VisualStudio\FISAcops";
+            string filePath = System.IO.Path.Combine(appLocation, "FISAcops", "Students", "students.json");
+            File.WriteAllText(filePath, json);
+        }
+
         public Students()
         {
             InitializeComponent();
+            List<Student> students = new List<Student>();
+
+            // Charger les étudiants à partir du fichier JSON
+            string appLocation = @"D:\Projets\VisualStudio\FISAcops";
+            string filePath = System.IO.Path.Combine(appLocation, "FISAcops", "Students", "students.json");
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                students = JsonSerializer.Deserialize<List<Student>>(json);
+            }
+
+            // Lier la liste d'étudiants à notre ListView
+            studentsListView.ItemsSource = students;
             Title = "Liste des élèves"; // Ajoutez cette ligne pour modifier le titre de la fenêtre
 
         }
     }
+
 }
