30398aa1a65a5533d76af0381f0b29a0ce6a54f4
TimDSi <theotime.dsilva1505@gmail.com>
2023-04-21 18:02:49 +0200

    Add project files.

---
+++ FISAcops/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FISAcops
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        private void BtnCall(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (MainWindow)Window.GetWindow(this);
+            mainWindow.frame.Navigate(new Call());
+        }
+
+        private void BtnCalender(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (MainWindow)Window.GetWindow(this);
+            mainWindow.frame.Navigate(new Calender());
+        }
+
+        private void BtnCreateGroup(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (MainWindow)Window.GetWindow(this);
+            mainWindow.frame.Navigate(new CreateGroup());
+        }
+
+        private void BtnGroups(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (MainWindow)Window.GetWindow(this);
+            mainWindow.frame.Navigate(new Groups());
+        }
+
+        private void BtnCreateStudent(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (MainWindow)Window.GetWindow(this);
+            mainWindow.frame.Navigate(new CreateStudent());
+        }
+
+        private void BtnStudents(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (MainWindow)Window.GetWindow(this);
+            mainWindow.frame.Navigate(new Students());
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FISAcops/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FISAcops
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            frame.NavigationService.Navigate(new MainPage());
+            // Définir la couleur de fond souhaitée
+            var backgroundColor = Brushes.LightBlue;
+
+            // Changer la couleur de fond de la fenêtre principale
+            Background = backgroundColor;
+        }
+    }
+}
