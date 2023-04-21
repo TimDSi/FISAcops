0e0ae39f694f4faa100cccd0e2bdc56f2d73f217
TimDSi <theotime.dsilva1505@gmail.com>
2023-04-21 18:57:41 +0200

    Création du formulaire de création d'un élève ainsi que du fichier les stockants.

--- FISAcops/Students/CreateStudent.xaml.cs
+++ FISAcops/Students/CreateStudent.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace FISAcops
 {
@@ -20,6 +23,39 @@ namespace FISAcops
     /// </summary>
     public partial class CreateStudent : Page
     {
+        private void SaveStudent_Click(object sender, RoutedEventArgs e)
+        {
+            string appLocation = @"D:\Projets\VisualStudio\FISAcops";
+            string filePath = System.IO.Path.Combine(appLocation, "FISAcops", "Students","students.json");
+            List<Student> students = new List<Student>();
+
+            // Charger les étudiants existants à partir du fichier JSON
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                students = JsonSerializer.Deserialize<List<Student>>(json);
+            }
+
+            // Ajouter un nouvel étudiant à la liste
+            students.Add(new Student
+            {
+                Nom = nomTextBox.Text,
+                Prenom = prenomTextBox.Text,
+                Mail = mailTextBox.Text,
+                Promotion = promoTextBox.Text
+            });
+
+            // Sauvegarder la liste complète d'étudiants dans le fichier JSON
+            var options = new JsonSerializerOptions
+            {
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                WriteIndented = true // Ajouter cette option pour formater le JSON de manière plus lisible
+            };
+            string output = JsonSerializer.Serialize(students, options);
+            File.WriteAllText(filePath, output, new UTF8Encoding(false));
+        }
+
+
         private void BtnMainPage(object sender, RoutedEventArgs e)
         {
             var mainWindow = (MainWindow)Window.GetWindow(this);
---
+++ FISAcops/Students/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace FISAcops
+{
+    internal class Student
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Mail { get; set; }
+        public string Promotion { get; set; }
+    }
+}
