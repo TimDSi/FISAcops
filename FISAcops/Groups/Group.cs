3e4e599222773ee4603eb5b83919e395736f2362
Timburio <Timburio@DESKTOP-QEE359B>
2023-04-23 11:30:18 +0200

    création du la classe Group

---
+++ FISAcops/Groups/Group.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FISAcops
+{
+    internal class Group
+    {
+        public string GroupName { get; set; }
+        public List<Student> StudentsList { get; set; }
+
+        public Group(string groupName, List<Student> studentsList)
+        {
+            GroupName = groupName;
+            StudentsList = studentsList;
+        }
+    }
+}
