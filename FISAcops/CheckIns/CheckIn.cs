fb8b40bb51aeb8c530b7653a8627ffb2dcc000a8
Timburio <Timburio@DESKTOP-QEE359B>
2023-04-30 18:42:10 +0200

    création de checkIns.cs

---
+++ FISAcops/CheckIns/CheckIn.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FISAcops.CheckIns
+{
+    internal class CheckIn
+    {
+    }
+}
