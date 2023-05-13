e2752f1797db6ca7cfc406973c303172e9be9e9f
Timburio <Timburio@DESKTOP-QEE359B>
2023-05-13 20:37:49 +0200

    ajout de StudentWithState dans factory

---
+++ FISAcops/StudentWithState.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FISAcops
+{
+    internal class StudentWithState : IStudent
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Mail { get; set; }
+        public string Promotion { get; set; }
+
+        public string State { get; set; }
+
+        public StudentWithState(string nom, string prenom, string mail, string promotion, string state)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Mail = mail;
+            Promotion = promotion;
+            State = state;
+        }
+
+
+        public void UpdateState(string newState)
+        {
+            State = newState;
+        }
+    }
+}
--- FISAcops/Students/StudentFactory.cs
+++ FISAcops/Students/StudentFactory.cs
@@ -8,11 +8,15 @@ namespace FISAcops
 {
     public static class StudentFactory
     {
-        public static IStudent CreateStudent(string nom, string prenom, string mail, string promotion, string? code)
+        public static IStudent CreateStudent(string nom, string prenom, string mail, string promotion, string? obj)
         {
-            if (code != null)
+            if (int.TryParse(obj, out int code))
             {
-                return new StudentWithCode(nom, prenom, mail, promotion, code);
+                return new StudentWithCode(nom, prenom, mail, promotion, code.ToString());
+            }
+            else if (!string.IsNullOrEmpty(obj))
+            {
+                return new StudentWithState(nom, prenom, mail, promotion, obj);
             }
             else
             {
