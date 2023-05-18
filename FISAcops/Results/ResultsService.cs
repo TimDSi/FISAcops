72d0af2cbb121c34be4fbb7301193d3240c4ca2e
Timburio <Timburio@DESKTOP-QEE359B>
2023-05-18 22:53:36 +0200

    Mise à jour de service result pour des dates propres à chaque result

--- FISAcops/Results/ResultsService.cs
+++ FISAcops/Results/ResultsService.cs
@@ -7,18 +7,18 @@ namespace FISAcops
 {
     static class ResultsService
     {
-        public static string resultsFilePath = Path.Combine(new Settings().resultsPath, "Results.json");
-
-        public static List<Result> LoadResultsFromJson()
+        public static List<Result> LoadResultsFromJson(string date)
         {
+            string resultsFilePath = Path.Combine(new Settings().resultsPath, "Results" + date + ".json");
             var json = File.ReadAllText(resultsFilePath);
             var results = JsonSerializer.Deserialize<List<Result>>(json);
             results ??= new List<Result>();
             return results;
         }
 
-        public static void SaveResultsToJson(List<Result> results)
+        public static void SaveResultsToJson(List<Result> results, string date)
         {
+            string resultsFilePath = Path.Combine(new Settings().resultsPath, "Results" + date + ".json");
             var json = JsonSerializer.Serialize(results, new JsonSerializerOptions { WriteIndented = true });
             File.WriteAllText(resultsFilePath, json);
         }
