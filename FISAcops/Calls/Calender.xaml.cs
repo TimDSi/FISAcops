26d8f03c06b327166a86b867c234b932d6d97ec6
Timburio <Timburio@DESKTOP-QEE359B>
2023-05-19 17:10:27 +0200

    optimisation de la page Calender

--- FISAcops/Calls/Calender.xaml.cs
+++ FISAcops/Calls/Calender.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FISAcops
 {
@@ -93,10 +83,7 @@ namespace FISAcops
             // Obtenir l'objet Call associé à la ligne du bouton cliqué
             var call = (Call)modifierButton.DataContext;
 
-            // Effectuer les actions de modification avec l'objet Call
-            // ...
-
-            // Exemple : Naviguer vers la page "EditCall" en passant l'objet Call en tant que paramètre
+            //Naviguer vers la page "EditCall" en passant l'objet Call en tant que paramètre
             var mainWindow = (MainWindow)Window.GetWindow(this);
             mainWindow.frame.Navigate(new EditCall(call));
         }
