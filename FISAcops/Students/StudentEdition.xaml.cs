61be1d3ff81714060f2e0ba56319ab84ed3bdfe6
Timburio <Timburio@DESKTOP-QEE359B>
2023-05-19 14:08:54 +0200

    optimisation de StudentEdition

--- FISAcops/Students/StudentEdition.xaml.cs
+++ FISAcops/Students/StudentEdition.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Globalization;
-using System.IO;
 using System.Linq;
 using System.Text;
-using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
--- FISAcops/Students/StudentFactory.cs
+++ FISAcops/Students/StudentFactory.cs
@@ -2,20 +2,28 @@
 {
     public static class StudentFactory
     {
-        public static IStudent CreateStudent(string nom, string prenom, string mail, string promotion, object obj)
+        public static IStudent CreateStudent(string nom, string prenom, string mail, string promotion, object? obj)
         {
-            if (obj is int code)
-            {
-                return new StudentWithCode(nom, prenom, mail, promotion, code);
-            }
-            else if (obj is string state)
-            {
-                return new StudentWithState(nom, prenom, mail, promotion, state);
+            IStudent student;
+            if (obj != null) {
+                if (obj is int code)
+                {
+                    student = new StudentWithCode(nom, prenom, mail, promotion, code);
+                }
+                else if (obj is string state)
+                {
+                    student = new StudentWithState(nom, prenom, mail, promotion, state);
+                }
+                else
+                {
+                    student = new Student(nom, prenom, mail, promotion);
+                }
             }
             else
             {
-                return new Student(nom, prenom, mail, promotion);
+                student = new Student(nom, prenom, mail, promotion);
             }
+            return student;
         }
     }
 }
